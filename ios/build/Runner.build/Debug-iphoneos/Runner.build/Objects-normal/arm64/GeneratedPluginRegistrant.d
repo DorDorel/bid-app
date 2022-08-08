96f8d72232d6c8f24622549b906f89982549a359
DorDorel <dorluzgarten@gmail.com>
2022-08-08 12:09:39 +0300

    ios build hot fix

---
+++ ios/build/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -0,0 +1,20 @@
+dependencies: /Users/dor/bid-app/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/dor/bid-app/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Modules/module.modulemap \
+  /Users/dor/bid-app/ios/Pods/Firebase/CoreOnly/Sources/module.modulemap \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterEngineGroup.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/dor/bid-app/ios/build/Debug-iphoneos/Flutter.framework/Headers/Flutter.h
---
+++ ios/build/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/dor/bid-app/ios/build/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
