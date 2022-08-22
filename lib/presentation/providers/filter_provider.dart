660ed7e545ccb875d88e851768a54c78c191e094
DorDorel <dorluzgarten@gmail.com>
2022-08-22 14:19:18 +0300

    new ui optimization

--- lib/presentation/providers/filter_provider.dart
+++ lib/presentation/providers/filter_provider.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/foundation.dart';
 
 class FilterProvider with ChangeNotifier {
-  int filterIndex = 0;
+  int _filterIndex = 0;
 
-  int get getFilterIndex => filterIndex;
+  int get getFilterIndex => _filterIndex;
   void updateFilterIndex({required int newIndex}) {
     if (newIndex < 0 || newIndex > 4) {
       return;
     }
-    filterIndex = newIndex;
+    _filterIndex = newIndex;
     notifyListeners();
-    print(getFilterIndex);
   }
 }
--- lib/presentation/widgets/home_widget_selector.dart
+++ lib/presentation/widgets/home_widget_selector.dart
@@ -1,11 +1,11 @@
-import 'package:bid/presentation/screens/bids/bids_archive_screen.dart';
 import 'package:bid/presentation/screens/bids/open_bids_screen.dart';
-import 'package:bid/presentation/screens/notification/notification_screen.dart';
-import 'package:bid/presentation/screens/user/user_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/filter_provider.dart';
+import '../screens/bids/bids_archive_screen.dart';
+import '../screens/notification/notification_screen.dart';
+import '../screens/user/user_profile.dart';
 
 class HomeWidgetSelector extends StatelessWidget {
   const HomeWidgetSelector({Key? key}) : super(key: key);
@@ -13,21 +13,20 @@ class HomeWidgetSelector extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final filterProvider = Provider.of<FilterProvider>(context);
-    if (filterProvider.getFilterIndex == 0) {
-      return OpenBidScreen();
+    switch (filterProvider.getFilterIndex) {
+      case 0:
+        return OpenBidScreen();
+      case 1:
+        return BidsArchiveScreen();
+      case 2:
+        return NotificationsScreen();
+      case 3:
+        return Text("three");
+      case 4:
+        return UserConfig();
+
+      default:
+        return OpenBidScreen();
     }
-    if (filterProvider.getFilterIndex == 1) {
-      return BidsArchiveScreen();
-    }
-    if (filterProvider.getFilterIndex == 2) {
-      return NotificationsScreen();
-    }
-    if (filterProvider.getFilterIndex == 3) {
-      return Text("three");
-    }
-    if (filterProvider.getFilterIndex == 4) {
-      return UserConfig();
-    }
-    return Text("zero");
   }
 }
